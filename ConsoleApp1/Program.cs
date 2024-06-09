1eedb9697e591af2310d11523bfff3ced6bb0dac
chjm44 <chc807000@naver.com>
2024-06-10 00:17:20 +0900

    console add

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hellow world");
+
+            string name = "hey";
+            Console.WriteLine(name);
         }
     }
 }
